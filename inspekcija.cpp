ab0c79ddcda50004af64b035581baec4b099a795
ffijuljani1 <73430835+ffijuljani1@users.noreply.github.com>
2021-12-10 11:04:29 +0100

    Create inspekcija.cpp
    666

---
+++ inspekcija.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class Taxi {
+  constexpr string ime = "DEDEIC";
+  
+  public void dispecer() {
+   cout << "Besim""; 
+  }
+  
+};
+
+int main() {
+ 
+  
+ return 0; 
+}
